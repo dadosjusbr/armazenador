1111b5612a85a77fd1dd8ba9fe58790c8130eb9d
Jefferson Neves <jefferson.rpn@gmail.com>
2022-01-14 16:23:09 -0300

    Refatora declaração de mocks de teste

--- main_test.go
+++ main_test.go
@@ -8,31 +8,27 @@ import (
 )
 
 func TestCalcCompletenessScore(t *testing.T) {
-	inputA := coleta.Metadados{
-		TemMatricula:   true,
-		TemLotacao:     true,
-		TemCargo:       true,
-		ReceitaBase:    coleta.Metadados_DETALHADO,
-		OutrasReceitas: coleta.Metadados_DETALHADO,
-		Despesas:       coleta.Metadados_DETALHADO,
-	}
-
-	inputB := coleta.Metadados{
-		TemMatricula:   false,
-		TemLotacao:     false,
-		TemCargo:       false,
-		ReceitaBase:    coleta.Metadados_AUSENCIA,
-		OutrasReceitas: coleta.Metadados_AUSENCIA,
-		Despesas:       coleta.Metadados_AUSENCIA,
-	}
-
 	data := []struct {
 		Desc     string
 		Input    coleta.Metadados
 		Expected float64
 	}{
-		{"Best", inputA, float64(1)},
-		{"Worst", inputB, float64(0)},
+		{"Best", coleta.Metadados{
+			TemMatricula:   true,
+			TemLotacao:     true,
+			TemCargo:       true,
+			ReceitaBase:    coleta.Metadados_DETALHADO,
+			OutrasReceitas: coleta.Metadados_DETALHADO,
+			Despesas:       coleta.Metadados_DETALHADO,
+		}, float64(1)},
+		{"Worst", coleta.Metadados{
+			TemMatricula:   false,
+			TemLotacao:     false,
+			TemCargo:       false,
+			ReceitaBase:    coleta.Metadados_AUSENCIA,
+			OutrasReceitas: coleta.Metadados_AUSENCIA,
+			Despesas:       coleta.Metadados_AUSENCIA,
+		}, float64(0)},
 	}
 
 	for _, d := range data {
@@ -45,29 +41,25 @@ func TestCalcCompletenessScore(t *testing.T) {
 }
 
 func TestCalcEasinessScore(t *testing.T) {
-	inputA := coleta.Metadados{
-		NaoRequerLogin:      true,
-		NaoRequerCaptcha:    true,
-		Acesso:              coleta.Metadados_ACESSO_DIRETO,
-		FormatoConsistente:  true,
-		EstritamenteTabular: true,
-	}
-
-	inputB := coleta.Metadados{
-		NaoRequerLogin:      false,
-		NaoRequerCaptcha:    false,
-		Acesso:              coleta.Metadados_NECESSITA_SIMULACAO_USUARIO,
-		FormatoConsistente:  false,
-		EstritamenteTabular: false,
-	}
-
 	data := []struct {
 		Desc     string
 		Input    coleta.Metadados
 		Expected float64
 	}{
-		{"Best", inputA, float64(1)},
-		{"Worst", inputB, float64(0)},
+		{"Best", coleta.Metadados{
+			NaoRequerLogin:      true,
+			NaoRequerCaptcha:    true,
+			Acesso:              coleta.Metadados_ACESSO_DIRETO,
+			FormatoConsistente:  true,
+			EstritamenteTabular: true,
+		}, float64(1)},
+		{"Worst", coleta.Metadados{
+			NaoRequerLogin:      false,
+			NaoRequerCaptcha:    false,
+			Acesso:              coleta.Metadados_NECESSITA_SIMULACAO_USUARIO,
+			FormatoConsistente:  false,
+			EstritamenteTabular: false,
+		}, float64(0)},
 	}
 
 	for _, d := range data {
